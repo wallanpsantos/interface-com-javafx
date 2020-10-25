459045dd7b9bbeb3e748a9e5c0a9c15d1a35edec
Wallan Pereira <wallanpereira09@gmail.com>
2020-10-24 21:25:26 -0300

    Interface de soma

--- src/gui/controller/ViewController.java
+++ src/gui/controller/ViewController.java
@@ -1,18 +1,39 @@
 package gui.controller;
 
+import java.util.Locale;
+
 import gui.util.AlertasUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
 
 	@FXML
-	private Button botao;
+	private TextField textPrimeiroNumero;
 
 	@FXML
-	public void onBotaoClique() {
-		AlertasUtils.mensagemAlerta("Titulo do alerta", "Cabeçalho do alerta", "Mensagem de texto no corpo",
-				AlertType.INFORMATION);
+	private TextField textSegundoNumero;
+
+	@FXML
+	private Label labelResultado;
+
+	@FXML
+	private Button buttonCalcular;
+
+	@FXML
+	public void onBotaoCalcular() {
+		try {
+			Locale.setDefault(Locale.US);
+			Double primeiroValor = Double.parseDouble(textPrimeiroNumero.getText());
+			Double segundoValor = Double.parseDouble(textSegundoNumero.getText());
+
+			Double soma = primeiroValor + segundoValor;
+			labelResultado.setText(String.format("%.2f", soma));
+		} catch (Exception e) {
+			AlertasUtils.mensagemAlerta("Erro de exceção", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
